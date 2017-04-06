860b73c3435ab9c5b80987be5227e849c2d32fe7
seu-tan <tanjun0724@163.com>
2017-04-06 21:38:13 +0800

    实现EWMA

---
+++ scikit/ewm.py
@@ -0,0 +1,24 @@
+import numpy as np
+import pandas as pd
+import time
+
+def numpy_ewma(data, window):
+    alpha = 2 /(window + 1.0)
+    alpha_rev = 1-alpha
+    n = data.shape[0]
+
+    pows = alpha_rev**(np.arange(n+1))
+
+    scale_arr = 1/pows[:-1]
+    offset = data[0]*pows[1:]
+    pw0 = alpha*alpha_rev**(n-1)
+
+    mult = data*pw0*scale_arr
+    cumsums = mult.cumsum()
+    out = offset + cumsums*scale_arr[::-1]
+    return out
+
+a = np.arange(2000)
+b = numpy_ewma(a,6)
+print(b)
+
